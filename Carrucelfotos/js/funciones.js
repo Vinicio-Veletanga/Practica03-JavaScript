31e3ee8e4c007a62c7de6a252f4bbba41c8f343c
Vinicio-Veletanga <52483286+Vinicio-Veletanga@users.noreply.github.com>
2019-11-19 15:31:26 -0500

    finalizado

--- Carrucelfotos/js/funciones.js
+++ Carrucelfotos/js/funciones.js
@@ -3,24 +3,6 @@ var par2
 var num = 0
 var participantes = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg'];
 
-function iniciar() {
-    console.log(participantes)
-    num = 0
-    for (var i = 0; i < 5; i++) {
-        participantes[i] = Math.floor(Math.random() * (10))
-    }
-    console.log(participantes)
-    par2 = participantes.slice(0, 5)
-    alert('Se escogieron las ' + par2.length + ' imagenes aleatorias')
-    console.log(par2)
-    console.log(num)
-    direccion = "images/" + par2[num] + ".jpg";
-    console.log(direccion);
-    document.getElementById("foto").src = direccion;
-    document.getElementById("previous").disabled = true;
-    document.getElementById("next").disabled = false;
-}
-
 
 function siguiente() {
     num++
@@ -40,6 +22,23 @@ function siguiente() {
     document.getElementById("foto").src = direccion;
 }
 
+function iniciar() {
+    console.log(participantes)
+    num = 0
+    for (var i = 0; i < 5; i++) {
+        participantes[i] = Math.floor(Math.random() * (10))
+    }
+    console.log(participantes)
+    par2 = participantes.slice(0, 5)
+    alert('Se escogieron las ' + par2.length + ' imagenes aleatorias')
+    console.log(par2)
+    console.log(num)
+    direccion = "images/" + par2[num] + ".jpg";
+    console.log(direccion);
+    document.getElementById("foto").src = direccion;
+    document.getElementById("previous").disabled = true;
+    document.getElementById("next").disabled = false;
+}
 function anterior() {
     num--
     if (num == 4) {
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# Actividades a desarrollar "
+Actividades a desarrollar 
+
+1. Crear un repositorio en GitHub con el nombre “Practica03 – Javascript” 
+
+
+2. Crear una carpeta para la solución de cada ejercicio antes mencionado 
+
+3. Realizar un commit y push por cada requerimiento de los puntos antes descritos. 
+
+4. Luego, se debe crear el archivo README del repositorio de GitHub. 
+
+5. Generar informe de los resultados en el formato de prácticas.
+ Debe incluir: 
+a. El desarrollo de cada uno de los requerimientos antes descritos. 
+b. La evidencia del correcto diseño de las páginas HTML usando CSS. Para lo cuál, se puede generar fotografías instantáneas (pantallazos). 
+c. La evidencia del correcto funcionamiento de cada una de las funciones de JavaScript 
+d. El informe debe incluir conclusiones apropiadas.  
+e. En el informe se debe incluir la información de GitHub (usuario y URL del repositorio de la práctica) 
+ 
+f. En el informe se debe incluir la firma digital del estudiante. 
+6. En el archivo README del repositorio debe constar la misma información del informe de resultados de la práctica que se indica en el punto anterior.
